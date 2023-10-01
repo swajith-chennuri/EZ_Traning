ef854a2fd3fe1bf7a662b153e589cc7adda69b41
swajith-chennuri <108970806+swajith-chennuri@users.noreply.github.com>
2023-10-01 15:44:29 +0530

    Delete day 4 directory

--- day 4/queue using pyhton method 2.py
+++
@@ -1,9 +0,0 @@
-from collections import deque
-l=deque()
-n=int(input("enter the no of elements = "))
-for i in range(n):
-    l.append(input("enter the element = "))
-print(l)
-for i in range(n):
-    print(l.popleft())
-print(l)
--- day 4/queue using pyhton.py
+++
@@ -1,8 +0,0 @@
-l=[]
-n=int(input("enter the no of elements = "))
-for i in range(n):
-    l.append(input("enter the element = "))
-print(l)
-for i in range(n):
-    print(l.pop(0))
-print(l)
